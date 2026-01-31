533ef4f52429b56c8f257aef62eba68e6bcac14d
Duong Nguyen Hoang <romanguyen110@gmail.com>
2026-01-31 20:24:30 +0100

    refactor: remove default semester JSON and simplify DefaultData function

--- internal/app/data.go
+++ internal/app/data.go
@@ -1,19 +1,7 @@
 package app
 
-import (
-	_ "embed"
-	"encoding/json"
-
-	"student-exams-manager/internal/storage"
-)
-
-//go:embed default_semester.json
-var defaultSemesterJSON []byte
+import "student-exams-manager/internal/storage"
 
 func DefaultData() storage.SemesterData {
-	var data storage.SemesterData
-	if err := json.Unmarshal(defaultSemesterJSON, &data); err != nil {
-		return storage.SemesterData{}
-	}
-	return data
+	return storage.SemesterData{}
 }
